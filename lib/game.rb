3bbf20a855fa2abf288423339a077cdaf70a23cf
galicians <galicians@gmail.com>
2014-09-24 19:06:59 +0100

    correction on merge

--- lib/game.rb
+++ lib/game.rb
@@ -4,19 +4,26 @@ class Game
 	def initialize
 	end
 
-def place_ship(ship,coordinates,grid)
+	def place_ship(ship,coordinates,grid)
 	# {coordinates[0]}"
 
-	ship.size
-	puts "this is our a: #{coordinates[0][0]}"
+	
+	puts "this is our a: #{coordinates[0][0] + 1}"
 	puts "this is our b: #{coordinates[0][1]}"
 
-	
-	while (coordinates[0][0] <= coordinates[0][1])
+	 while (coordinates[0][0] + 1 <= 10)
+	 	puts "inside our loop #{coordinates[0][0] + 1}"
 
-		puts "our coordinates is #{coordinates[0][0]} and #{coordinates[0][1]}"
-		# grid.grid[coordinates[0][0] + 1,coordinates[0][1]] = '[S]'
-		coordinates[0][1] 
+	 end
+
+
+	# while (coordinates[0][0] + 1 <= coordinates[0][1])
+
+	# 	puts "our coordinates is #{coordinates[0][0]} and #{coordinates[0][1]}"
+	# 	# grid.grid[coordinates[0][0] + 1,coordinates[0][1]] = '[S]'
+		
+	# 	puts "#{coordinates[0][1]}"
+	# end
 
 	end
 	
@@ -26,7 +33,7 @@ def place_ship(ship,coordinates,grid)
 	# # {grid.grid[coordinates[0]]}"
 	# # # puts "just ensuring we can see grid from the game class #{@grid}"
 
-end
+
 	def target(co1, co2, grid)
 		grid.grid[[co1, co2]] = "[O]"
 	end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,7 +5,6 @@ require 'square'
 describe Game do 
 
 
-	describe Game do
 	let(:game) { Game.new }
 	let(:submarine) { Submarine.new }
 	let(:grid) { Grid.new }
@@ -18,16 +17,16 @@ describe Game do
 	end
 
 	it 'should allow player to target coordinate' do 
-		game = Game.new
-		grid = Grid.new
+		# game = Game.new
+		# grid = Grid.new
 		game.target(1, 1, grid)
 		expect(grid.grid[[1, 1]]).to eq("[O]")
 
 	end
 
 	it 'should change target square status to hit when targetted' do
-		game = Game.new
-		grid = Grid.new
+		# game = Game.new
+		# grid = Grid.new
 		expect{game.target(1, 1, grid)}.to change{grid.grid[[1, 1]]}.to eq("[O]")
 
 	end
