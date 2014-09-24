9e24d27bc0a4aabc27dcad73e89afc5cef35adb7
galicians <galicians@gmail.com>
2014-09-24 19:40:42 +0100

    places ships in horizontal

--- lib/game.rb
+++ lib/game.rb
@@ -5,16 +5,30 @@ class Game
 	end
 
 	def place_ship(ship,coordinates,grid)
-	# {coordinates[0]}"
 
 	
 	puts "this is our a: #{coordinates[0][0] + 1}"
 	puts "this is our b: #{coordinates[0][1]}"
 
-	 while (coordinates[0][0] + 1 <= 10)
-	 	puts "inside our loop #{coordinates[0][0] + 1}"
-
+	 while (coordinates[0][0] <= coordinates[0][1])
+	 	# puts "inside our loop #{coordinates[0][0] + 1}"
+	 	while (coordinates[1][0] <= coordinates[1][1])
+		 	puts "#{coordinates[0][0]}  and #{coordinates[1][0]}"
+		 	grid.grid[[coordinates[0][0],coordinates[1][0]]] = '[S]'
+		 	coordinates[1][0] += 1
+	 	end
+	 	coordinates[0][0] += 1
 	 end
+
+
+	 
+	 # puts "second loop"
+
+	 #  while (coordinates[1][0] <= coordinates[1][1])
+	 # 	puts "inside our loop #{coordinates[1][0]}"
+	 # 	coordinates[1][0] += 1
+
+	 # end
 
 
 	# while (coordinates[0][0] + 1 <= coordinates[0][1])
