e21d8778ac67dbb8c083557589580619e550c79e
galicians <galicians@gmail.com>
2014-09-24 18:40:44 +0100

    working on place ship method

--- lib/game.rb
+++ lib/game.rb
@@ -5,10 +5,26 @@ class Game
 	end
 
 def place_ship(ship,coordinates,grid)
-	puts "this is the formate of coordinates #{coordinates[0]}"
-	grid.grid[coordinates[0]] = '[S]'
-	puts "this should be the square [1,1]: #{grid.grid[coordinates[0]]}"
-	# # puts "just ensuring we can see grid from the game class #{@grid}"
+	# {coordinates[0]}"
+
+	ship.size
+	puts "this is our a: #{coordinates[0][0]}"
+	puts "this is our b: #{coordinates[0][1]}"
+
+	
+	while (coordinates[0][0] <= coordinates[0][1])
+
+		puts "our coordinates is #{coordinates[0][0]} and #{coordinates[0][1]}"
+		# grid.grid[coordinates[0][0] + 1,coordinates[0][1]] = '[S]'
+		coordinates[0][1] 
+
+	end
+	
+	# # grid.grid[coordinates[0]] = '[S]'
+	# # grid.grid[coordinates[1]] = '[S]'
+	
+	# # {grid.grid[coordinates[0]]}"
+	# # # puts "just ensuring we can see grid from the game class #{@grid}"
 
 end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,8 +7,10 @@ describe Game do
 	let(:grid) { Grid.new }
 
 	it "should place a ship" do
-		game.place_ship(submarine,[[1,1],[1,3]],grid)
+		game.place_ship(submarine,[[1,1],[1,5]],grid)
 		grid.display
+		# [1,2],[5,2] -- >[2,2][3,2][4,2] -->[a,b],[a+1,b]
+		# [3,2],[7,2] -- >[4,2][5,2][6,2] -->[a,b],[a+1,b]
 	end
 
 end
